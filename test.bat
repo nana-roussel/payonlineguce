e2e37f9bee85513296573fe60da9e4ddf68922cc
nana-roussel <rousseltchanko@gmail.com>
2021-07-12 09:37:26 +0100

    test.bat

---
+++ test.bat
@@ -0,0 +1,2 @@
+cd CUserstchankoDownloadsCompressedKatalon_Studio_Engine_Windows_64-8.0.5
+katalonc -noSplash -runMode=console -projectPath=CUserstchankogitpayonlinegucepayonlineguce.prj -retry=0 -testSuitePath=Test Suitessuite du test de fonctionnement -executionProfile=default -browserType=Chrome -apiKey=babd032b-eac4-44aa-bd7c-7a6d76ed1fe1 --config -proxy.auth.option=NO_PROXY -proxy.system.option=NO_PROXY -proxy.system.applyToDesiredCapabilities=true
